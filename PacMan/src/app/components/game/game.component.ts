986d8bf3101bbb1b8baf9d1544f750303cb2965b
Kristiyan Kostadinov <kristiqn57@gmail.com>
2023-08-19 11:30:36 +0300

    Added game screen

--- PacMan/src/app/components/game/game.component.ts
+++ PacMan/src/app/components/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  public pacmanPosition = { x: 110, y: 190 };
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const stepSize =  5;
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.pacmanPosition.x -= stepSize;
+        break;
+      case 'ArrowUp':
+        this.pacmanPosition.y -= stepSize;
+        break;
+      case 'ArrowRight':
+        this.pacmanPosition.x += stepSize;
+        break;
+      case 'ArrowDown':
+        this.pacmanPosition.y += stepSize;
+        break;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
